50dc433efbfcfbd3d4ac4f0d7f5133e5b631a87b
Juhyun <juhyun_dev@naver.com>
2023-01-13 15:43:06 +0900

    Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = []; //빈 array 생성. toDo를 배열로 보관하기 위함.
+
+function saveToDos() {
+  // toDos array를 localStorage에 집어넣음
+  localStorage.setItem("todos", JSON.stringify(toDos)); // ("key", value)
+  // JSON.stringify -> 객체든 배열이든 모두 문자열로 만들어줌.
+  // localStorage에 저장할 때, array 는 저장할 수 없다. 오직 텍스트만 저장할 수 있다.
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -23,7 +32,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo); // toDos 라는 array에 newTodo를 푸쉬함.
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
