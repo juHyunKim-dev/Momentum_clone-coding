f78a8ad9f19f814ba32b3a33344fce0aa14331b9
Juhyun <juhyun_dev@naver.com>
2023-01-15 21:14:41 +0900

    2023-01-15

--- js/weather.js
+++ js/weather.js
@@ -5,14 +5,14 @@ function onGeoOk(position) {
   const lon = position.coords.longitude;
   console.log("You live in", lat, lon);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  fetch(url)
+  fetch(url) // JS가 url을 부름
     .then((response) => response.json())
     .then((data) => {
       const weather = document.querySelector("#weather span:first-child");
       const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main} / ${data.main.temp}℃`;
-    }); // JS가 url을 부름
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
